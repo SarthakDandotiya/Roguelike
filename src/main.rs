7888a7e1813879707eeaa2883bdd57d3ab82a84d
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-05-25 03:55:29 +0530

    Field-of-view and exploration

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use tcod::colors::*;
 use tcod::console::*;
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
+use tcod::map::{FovAlgorithm, Map as FovMap}; // Renaming the method Map as it shall conflict with our own dngeon map type
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -18,11 +19,25 @@ const ROOM_MIN_SIZE: i32 = 6;
 const MAX_ROOMS: i32 = 30;
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_LIGHT_WALL: Color = Color {
+    r: 130,
+    g: 110,
+    b: 50,
+};
 const COLOR_DARK_GROUND: Color = Color {
     r: 50,
     g: 50,
     b: 150,
 };
+const COLOR_LIGHT_GROUND: Color = Color {
+    r: 200,
+    g: 180,
+    b: 50,
+};
+
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
+const FOV_LIGHT_WALLS: bool = true;
+const TORCH_RADIUS: i32 = 10;
 
 type Map = Vec<Vec<Tile>>;
 
@@ -30,6 +45,7 @@ type Map = Vec<Vec<Tile>>;
 struct Tile {
     blocked: bool,
     block_sight: bool,
+    explored: bool,
 }
 
 impl Tile {
@@ -37,6 +53,7 @@ impl Tile {
         Tile {
             blocked: false,
             block_sight: false,
+            explored: false,
         }
     }
 
@@ -44,6 +61,7 @@ impl Tile {
         Tile {
             blocked: true,
             block_sight: true,
+            explored: false,
         }
     }
 }
@@ -221,17 +239,46 @@ fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
     false
 }
 
-fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+fn render_all(
+    root: &mut Root,
+    con: &mut Offscreen,
+    objects: &[Object],
+    map: &mut Map,
+    fov_map: &mut FovMap,
+    fov_recompute: bool,
+) {
+    if fov_recompute {
+        // recompute FOV if needed (the player moved or something)
+        let player = &objects[0];
+        fov_map.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
+    }
+
+    // go through all tiles, and set their background color
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
+            let visible = fov_map.is_in_fov(x, y);
             let wall = map[x as usize][y as usize].block_sight;
-            if wall {
-                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            let color = match (visible, wall) {
+                // outside of field of view:
+                (false, true) => COLOR_DARK_WALL,
+                (false, false) => COLOR_DARK_GROUND,
+                // inside fov:
+                (true, true) => COLOR_LIGHT_WALL,
+                (true, false) => COLOR_LIGHT_GROUND,
+            };
+
+            let explored = &mut map[x as usize][y as usize].explored;
+            if visible {
+                // since it's visible, explore it
+                *explored = true;
+            }
+            if *explored {
+                // show explored tiles only (any visible tile is explored already)
+                con.set_char_background(x, y, color, BackgroundFlag::Set);
             }
         }
     }
+
     // draw all objects in the list
     for object in objects {
         object.draw(con);
@@ -254,7 +301,7 @@ fn main() {
     let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     // generate map (it isn't drawn onto the screen yet)
-    let (map, (player_x, player_y)) = make_map();
+    let (mut map, (player_x, player_y)) = make_map();
 
     // object representing the player
     // putting the player inside the first room
@@ -266,6 +313,21 @@ fn main() {
     // Holds all objects in an array
     let mut objects = [player, npc];
 
+    let mut fov_map = FovMap::new(MAP_WIDTH, MAP_HEIGHT);
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            fov_map.set(
+                x,
+                y,
+                !map[x as usize][y as usize].block_sight,
+                !map[x as usize][y as usize].blocked,
+            );
+        }
+    }
+
+    // computing the "FOV" manually the first time
+    let mut previous_player_position = (-1, -1);
+
     while !root.window_closed() {
         // Clear screen of previous frame
         con.clear();
@@ -275,12 +337,21 @@ fn main() {
         }
 
         // render the screen
-        render_all(&mut root, &mut con, &objects, &map);
+        let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+        render_all(
+            &mut root,
+            &mut con,
+            &objects,
+            &mut map,
+            &mut fov_map,
+            fov_recompute,
+        );
 
         root.flush();
 
         // handle keys and exit game if needed
         let player = &mut objects[0];
+        previous_player_position = (player.x, player.y);
         let exit = handle_keys(&mut root, player, &map);
         if exit {
             break;
