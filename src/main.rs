12b692d6c9bbbcabcc6046893e3b53dc653a2730
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-05-24 22:24:07 +0530

    The object and the map

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,81 @@ const SCREEN_HEIGHT: i32 = 50;
 
 const LIMIT_FPS: i32 = 30; // 30 Frames per Second Limit
 
-fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+
+type Map = Vec<Vec<Tile>>;
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+// Object to reuse for anything representable by a character on the screen
+#[derive(Debug)]
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    // move by the given amount, if the destination is not blocked
+    pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
+        if !map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
+    }
+
+    /// set the color and then draw the character that represents this object at its position
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+fn make_map() -> Map {
+    // fill map with "unblocked" tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // place two pillars to test the map
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
     let key = root.wait_for_keypress(true);
     match key {
         Key {
@@ -23,13 +97,35 @@ fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool 
         Key { code: Escape, .. } => return true, // exit game
 
         // movement keys
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
-        _ => {} // default
+        Key { code: Up, .. } => player.move_by(0, -1, map),
+        Key { code: Down, .. } => player.move_by(0, 1, map),
+        Key { code: Left, .. } => player.move_by(-1, 0, map),
+        Key { code: Right, .. } => player.move_by(1, 0, map),
+
+        _ => {}
     }
+
     false
+}
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+    // draw all objects in the list
+    for object in objects {
+        object.draw(con);
+    }
+
+    // blit the contents of "con" to the root console
+    blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
 }
 
 fn main() {
@@ -42,18 +138,36 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
+
+    // Create player object
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+
+    // Create NPC object
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW); // Non player characters shall be yellow
+
+    // Holds all objects in an array
+    let mut objects = [player, npc];
+
+    // generate map (at this point it's not drawn to the screen)
+    let map = make_map();
 
     while !root.window_closed() {
-        root.set_default_foreground(WHITE);
-        root.clear();
-        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        // Clear screen of previous frame
+        con.clear();
+
+        for object in &objects {
+            object.draw(&mut con);
+        }
+
+        // render the screen
+        render_all(&mut root, &mut con, &objects, &map);
+
         root.flush();
-        root.wait_for_keypress(true);
 
         // handle keys and exit game if needed
-        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut root, player, &map);
         if exit {
             break;
         }
